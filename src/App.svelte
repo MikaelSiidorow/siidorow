e6a2c31ab16e368dff95644e3b8c9993704e3732
Mikael Siidorow <mikael.siidorow@aalto.fi>
2022-12-20 14:26:42 +0200

    :tada: Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+  <!-- nav -->
+  <!-- hero -->
+  <!-- cards -->
+  <!-- about w/ timeline -->
+  <!-- footer -->
+</main>
+
+<style>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './styles/reset.css'
+import './styles/theme.css'
+import App from './App.svelte'
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
