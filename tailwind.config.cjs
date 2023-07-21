f0f708f155f590c18e864f36b320f513cc86a875
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-07-21 20:59:13 +0300

    chore: upgrade deps, format code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,18 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent': {
-          'main': '#26bb98',
-          'alt': '#bb2649',
-        }
+        accent: {
+          main: "#26bb98",
+          alt: "#bb2649",
+        },
       },
       boxShadow: {
         solid: "4px 4px #000",
-        'solid-dark': "4px 4px #fff",
+        "solid-dark": "4px 4px #fff",
         repeat: "24px 24px #26bb98, 24px 24px 0 4px #000",
-        'repeat-dark': "24px 24px #bb2649, 24px 24px 0 4px #fff"
+        "repeat-dark": "24px 24px #bb2649, 24px 24px 0 4px #fff",
       },
-    }
+    },
   },
   plugins: [],
-}
+};
